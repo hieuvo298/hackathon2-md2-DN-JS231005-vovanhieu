3c1ce09d66ed6e5d7a3691c95787728be0022248
hieuvo298 <hieuvv29899@gmail.com>
2023-12-05 17:18:56 +0700

    hackathon-advance

--- dist/index.js
+++ dist/index.js
@@ -1,84 +1,85 @@
 "use strict";
-const blogList = [
-    {
-        title: "Công nghệ ReactJS",
-        content: "ReactJS là 1 thư viện của Javascript dùng để xây dựng các ứng dụng single page application",
-        id: 1,
-    },
-];
-if (!JSON.parse(localStorage.getItem("blogList"))) {
-    localStorage.setItem("blogList", JSON.stringify(blogList));
+const todoList = [];
+if (!JSON.parse(localStorage.getItem("todoList"))) {
+    localStorage.setItem('todoList', JSON.stringify(todoList));
 }
-function addBlog() {
-    const inputTittleElement = document.querySelector("#tittle");
-    const inputContentElement = document.querySelector("#content");
-    const inputTittleValue = inputTittleElement.value;
-    const inputContentValue = inputContentElement.value;
-    if (!inputTittleValue || !inputContentValue) {
-        return alert("Tiêu đề và nội dung không được để trống.");
+function renderTodo() {
+    const localData = JSON.parse(localStorage.getItem('todoList') || '[]');
+    const listTodo = document.querySelector('.list-todo');
+    listTodo.innerHTML = "";
+    if (localData.length === 0) {
+        listTodo.innerHTML = `
+      <img src="../img/360_F_586210337_WOGOw0l7raEB8F61Muc4hWbvVcyQdk9Z.jpg" alt="anh">
+    `;
     }
-    const dataList = JSON.parse(localStorage.getItem("blogList")) || [];
-    const newBlog = {
-        title: inputTittleValue,
-        content: inputContentValue,
-        id: dataList.length + 1,
-    };
-    dataList.push(newBlog);
-    localStorage.setItem("blogList", JSON.stringify(dataList));
-    renderBlog();
-    inputTittleElement.value = "";
-    inputContentElement.value = "";
-}
-function openModal(id) {
-    localStorage.setItem("currentBlogId", id.toString());
-    const commentModal = document.getElementById("commentModal");
-    if (commentModal) {
-        commentModal.style.display = "block";
-    }
-}
-function closeModal() {
-    const commentModal = document.getElementById("commentModal");
-    if (commentModal) {
-        commentModal.style.display = "none";
-    }
-}
-function addComment() {
-    const commentInput = document.getElementById("commentInput");
-    const commentText = (commentInput === null || commentInput === void 0 ? void 0 : commentInput.value) || "";
-    if (!commentText) {
-        return alert("Bình luận không được để trống.");
-    }
-    const currentBlogId = parseInt(localStorage.getItem("currentBlogId") || "0");
-    const dataBlog = JSON.parse(localStorage.getItem("blogList")) || [];
-    const currentBlog = dataBlog.find(blog => blog.id === currentBlogId);
-    if (currentBlog) {
-        currentBlog.comments = currentBlog.comments || [];
-        currentBlog.comments.push(commentText);
-        localStorage.setItem("blogList", JSON.stringify(dataBlog));
-        closeModal();
-        renderBlog();
-    }
-}
-function comment(id) {
-    openModal(id);
-}
-function renderBlog() {
-    const dataBlog = JSON.parse(localStorage.getItem("blogList")) || [];
-    const listBlog = document.querySelector(".content-blog");
-    if (listBlog) {
-        listBlog.innerHTML = "";
-        dataBlog.forEach((item) => {
-            const commentCount = item.comments ? item.comments.length : 0;
-            listBlog.innerHTML += `
-        <div class="new-content">
-          <h4>${item.title}</h4>
-          <p>${item.content}</p>
-          <button onclick="comment(${item.id})">Bình luận</button>
-          <p > ${commentCount} bình luận</p>
-          <p>${item.comments ? item.comments : ""}</p>
-        </div>
+    else {
+        localData.forEach(function (item, index) {
+            const countCompeleteWork = [item.content].length;
+            console.log(item);
+            const inCompeleteWork = item.status ? item.status : 0;
+            listTodo.innerHTML += `
+        <div class="todo-list">
+          <div class="checkbox">
+            <input type="checkbox" onclick="updateTodo(${index})" ${item.status ? 'checked' : ''}>
+            <p style="text-decoration:${item.status ? 'line-through' : 'none'}">${item.content}</p>
+          </div>
+          <div class="icon">
+            <i onclick="editToDo(${index})" class="fa-solid fa-pen" style="color: #ffa200;"></i>
+            <i onclick="deleteTodo(${index})" class="fa-solid fa-trash" style="color: #e10505;"></i>
+          </div>
+          </div>
+          <div class="compelete-work">
+          <p>Công việc đã hoàn thành${inCompeleteWork}/${countCompeleteWork}</p>
+          </div>
       `;
         });
     }
 }
-renderBlog();
+renderTodo();
+function addTodo() {
+    const inputElement = document.querySelector('#input-form');
+    const inputValue = inputElement.value;
+    const newTodo = {
+        content: inputValue,
+        status: 0
+    };
+    if (!inputValue) {
+        alert('Tên công việc không được để trống.');
+        return;
+    }
+    const localData = JSON.parse(localStorage.getItem('todoList') || '[]');
+    localData.push(newTodo);
+    localStorage.setItem('todoList', JSON.stringify(localData));
+    inputElement.value = "";
+    renderTodo();
+}
+function deleteTodo(index) {
+    const dataLocal = JSON.parse(localStorage.getItem('todoList') || '[]');
+    const item = dataLocal[index];
+    const confirmDelete = confirm(`Bạn có chắc muốn xóa công việc "${item.content}" không?`);
+    if (confirmDelete) {
+        dataLocal.splice(index, 1);
+        localStorage.setItem("todoList", JSON.stringify(dataLocal));
+        renderTodo();
+    }
+    ;
+}
+function editToDo(index) {
+    const dataList = JSON.parse(localStorage.getItem("todoList")) || [];
+    const currentContent = dataList[index].content;
+    const updatedContent = prompt("Nhập công việc muôn sửa:", currentContent);
+    if (!updatedContent) {
+        return alert('Tên công việc không được để trống.');
+    }
+    if (updatedContent !== null) {
+        dataList[index].content = updatedContent;
+        localStorage.setItem("todoList", JSON.stringify(dataList));
+        renderTodo();
+    }
+}
+function updateTodo(index) {
+    const dataList = JSON.parse(localStorage.getItem("todoList")) || [];
+    dataList[index].status = dataList[index].status ? 0 : 1;
+    localStorage.setItem("todoList", JSON.stringify(dataList));
+    renderTodo();
+}
--- src/index.ts
+++ src/index.ts
@@ -1,107 +1,91 @@
-interface Blog {
-  title: string;
+interface TodoItem {
   content: string;
-  id: number;
-  comments?: string[];
+  status: number;
+}
+const todoList: TodoItem[] = [];
+
+if (!JSON.parse(localStorage.getItem("todoList") as string )) {
+  localStorage.setItem('todoList', JSON.stringify(todoList));
 }
 
-const blogList: Blog[] = [
-  {
-    title: "Công nghệ ReactJS",
-    content: "ReactJS là 1 thư viện của Javascript dùng để xây dựng các ứng dụng single page application",
-    id: 1,
-  },
-];
-
-if (!JSON.parse(localStorage.getItem("blogList") as string)) {
-  localStorage.setItem("blogList", JSON.stringify(blogList));
-}
-function addBlog(): void {
-  const inputTittleElement: HTMLInputElement = document.querySelector(
-    "#tittle"
-  ) as HTMLInputElement;
-  const inputContentElement: HTMLInputElement = document.querySelector(
-    "#content"
-  ) as HTMLInputElement;
-  const inputTittleValue: string = inputTittleElement.value;
-  const inputContentValue: string = inputContentElement.value;
-
-  if (!inputTittleValue || !inputContentValue) {
-    return alert("Tiêu đề và nội dung không được để trống.");
-  }
-  const dataList: Blog[] =
-    JSON.parse(localStorage.getItem("blogList") as string) || [];
-  const newBlog: Blog = {
-    title: inputTittleValue,
-    content: inputContentValue,
-    id: dataList.length + 1,
-  };
-  dataList.push(newBlog);
-  localStorage.setItem("blogList", JSON.stringify(dataList));
-  renderBlog();
-  inputTittleElement.value = "";
-  inputContentElement.value = "";
-}
-function openModal(id: number): void {
-  localStorage.setItem("currentBlogId", id.toString());
-  const commentModal = document.getElementById("commentModal");
-  if (commentModal) {
-    commentModal.style.display = "block";
-  }
-}
-
-function closeModal(): void {
-  const commentModal = document.getElementById("commentModal");
-  if (commentModal) {
-    commentModal.style.display = "none";
-  }
-}
-
-function addComment(): void {
-  const commentInput: HTMLInputElement | null = document.getElementById("commentInput") as HTMLInputElement;
-  const commentText: string = commentInput?.value || "";
-
-  if (!commentText) {
-    return alert("Bình luận không được để trống.");
-  }
-
-  const currentBlogId: number = parseInt(localStorage.getItem("currentBlogId") || "0");
-  const dataBlog: Blog[] = JSON.parse(localStorage.getItem("blogList") as string) || [];
-
-  const currentBlog = dataBlog.find(blog => blog.id === currentBlogId);
-
-  if (currentBlog) {
-    currentBlog.comments = currentBlog.comments || [];
-    currentBlog.comments.push(commentText);
-
-    localStorage.setItem("blogList", JSON.stringify(dataBlog));
-    closeModal();
-    renderBlog();
-  }
-}
-
-function comment(id: number): void {
-  openModal(id);
-}
-
-function renderBlog(): void {
-  const dataBlog: Blog[] = JSON.parse(localStorage.getItem("blogList") as string) || [];
-  const listBlog: HTMLElement | null = document.querySelector(".content-blog") ;
-  if(listBlog){
-  listBlog.innerHTML = "";
-  dataBlog.forEach((item: Blog) => {
-    const commentCount = item.comments ? item.comments.length : 0;
-      listBlog.innerHTML += `
-        <div class="new-content">
-          <h4>${item.title}</h4>
-          <p>${item.content}</p>
-          <button onclick="comment(${item.id})">Bình luận</button>
-          <p > ${commentCount} bình luận</p>
-          <p>${item.comments?item.comments:""}</p>
-        </div>
+function renderTodo() {
+  const localData: TodoItem[] = JSON.parse(localStorage.getItem('todoList') || '[]');
+  const listTodo = document.querySelector('.list-todo') as HTMLElement;
+  listTodo.innerHTML = "";
+  if ( localData.length === 0) {
+    listTodo.innerHTML = `
+      <img src="../img/360_F_586210337_WOGOw0l7raEB8F61Muc4hWbvVcyQdk9Z.jpg" alt="anh">
+    `; 
+  } else {
+    localData.forEach(function (item, index) {
+      const countCompeleteWork=[item.content].length
+      console.log(item);
+      const inCompeleteWork=item.status?item.status:0
+      listTodo.innerHTML += `
+        <div class="todo-list">
+          <div class="checkbox">
+            <input type="checkbox" onclick="updateTodo(${index})" ${item.status ? 'checked' : ''}>
+            <p style="text-decoration:${item.status ? 'line-through' : 'none'}">${item.content}</p>
+          </div>
+          <div class="icon">
+            <i onclick="editToDo(${index})" class="fa-solid fa-pen" style="color: #ffa200;"></i>
+            <i onclick="deleteTodo(${index})" class="fa-solid fa-trash" style="color: #e10505;"></i>
+          </div>
+          </div>
+          <div class="compelete-work">
+          <p>Công việc đã hoàn thành${inCompeleteWork}/${countCompeleteWork}</p>
+          </div>
       `;
     });
-  }}
-renderBlog();  
+  }
+}
+renderTodo()
+function addTodo() {
+  const inputElement = document.querySelector('#input-form') as HTMLInputElement;
+  const inputValue = inputElement.value;
+  const newTodo: TodoItem = {
+      content: inputValue,
+      status: 0
+  };
+  if (!inputValue) {
+      alert('Tên công việc không được để trống.');
+      return;
+  }
 
+  const localData: TodoItem[] = JSON.parse(localStorage.getItem('todoList') || '[]');
+  localData.push(newTodo);
+  localStorage.setItem('todoList', JSON.stringify(localData));
+  inputElement.value = "";
+  renderTodo();
+}
+function deleteTodo(index: number) {
+  const dataLocal: TodoItem[] = JSON.parse(localStorage.getItem('todoList') || '[]');
+  const item = dataLocal[index];
+  const confirmDelete = confirm(`Bạn có chắc muốn xóa công việc "${item.content}" không?`);
+
+  if (confirmDelete) {
+    dataLocal.splice(index, 1); 
+    localStorage.setItem("todoList", JSON.stringify(dataLocal));
+    renderTodo();
+    };     
+}
+function editToDo(index: number): void {
+  const dataList: TodoItem[] = JSON.parse(localStorage.getItem("todoList") as string) || [];
+  const currentContent: string = dataList[index].content;
+  const updatedContent: string | null  = prompt("Nhập công việc muôn sửa:", currentContent);
+  if(!updatedContent){
+      return alert('Tên công việc không được để trống.'); 
+  }
+  if (updatedContent!== null ) {
+    dataList[index].content = updatedContent;
+    localStorage.setItem("todoList", JSON.stringify(dataList));
+    renderTodo();
+  }
+} 
+function updateTodo(index: number): void {
+  const dataList: TodoItem[] = JSON.parse(localStorage.getItem("todoList") as string) || [];
+  dataList[index].status = dataList[index].status ? 0 : 1; 
+  localStorage.setItem("todoList", JSON.stringify(dataList));
+  renderTodo();
+}
 
